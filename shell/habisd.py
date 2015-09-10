5bb22a517fa7b74b3a840ae05e5c30d378991147
Andrew J. Hesford <ajh@sideband.org>
2015-09-09 21:12:04 -0400

    Clean up habisd.

--- shell/habisd.py
+++ shell/habisd.py
@@ -1,19 +1,62 @@
 #!/usr/bin/env python
 
+import sys
+
 from twisted.spread import pb
 from twisted.internet import reactor
 
-from habis import wrappers
+from habis.habiconf import HabisConfigError, HabisConfigParser
 from habis.conductor import HabisConductor
 
-def main():
-	HabisConductor.registerWrapper("echo", "echo")
-	HabisConductor.registerWrapper("cat", "cat")
-	HabisConductor.registerWrapper("sleep", "sleep")
-	port = reactor.listenTCP(8088, pb.PBServerFactory(HabisConductor()))
-	print 'Listening on %s' % port.getHost()
-	reactor.run()
+def usage(progname):
+	print >> sys.stderr, 'USAGE: %s <configuration>' % progname
+
+
+def configureConductor(config):
+	csec = 'conductor'
+
+	# Grab the commands to wrap, if possible
+	try:
+		wrappers = config.getlist(csec, 'wrappers',
+				mapper=lambda x: x.split(':', 1))
+	except Exception as e:
+		err = 'Configuration must specify wrappers in [%s]' % csec
+		raise HabisConfigError.fromException(err, e)
+
+	# Grab the port on which to listen
+	try:
+		port = config.getint(csec, 'port', failfunc=lambda: 8088)
+		addr = config.get(csec, 'address', failfunc=lambda: '')
+	except Exception as e:
+		err = 'Invalid optional address and port specification in [%s]' % csec
+		raise HabisConfigError.fromException(err, e)
+
+	# Register the commands to wrap
+	for func, cmd in wrappers:
+		HabisConductor.registerWrapper(func, cmd)
+
+	# Listen on the desired port and address
+	factory = pb.PBServerFactory(HabisConductor())
+	lport = reactor.listenTCP(port, factory, interface=addr)
+
+	return lport
 
 
 if __name__ == "__main__":
-	main()
+	if len(sys.argv) != 2:
+		usage(sys.argv[0])
+		sys.exit(1)
+
+	try:
+		# Read the configuration file
+		config = HabisConfigParser.fromfile(sys.argv[1])
+	except:
+		print >> sys.stderr, 'ERROR: could not load configuration file %s' % sys.argv[1]
+		usage(sys.argv[0])
+		sys.exit(1)
+
+	# Configure the server
+	port = configureConductor(config)
+	# Fire the reactor to launch the server
+	print 'Listening on %s' % port.getHost()
+	reactor.run()
