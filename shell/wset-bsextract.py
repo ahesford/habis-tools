7da2bff164c71498bd19104315243272181ade45
Andrew J. Hesford <ajh@sideband.org>
2016-01-20 13:36:34 -0500

    Add some convenience scripts to pull data from WaveformSet files.

---
+++ shell/wset-bsextract.py
@@ -0,0 +1,26 @@
+#!/opt/python-2.7.9/bin/python
+
+import os, sys
+from habis.formats import WaveformSet
+from habis.habiconf import matchfiles, buildpaths
+
+def usage(progname=None, fatal=True):
+	progname = progname or sys.argv[0]
+	print >> sys.stderr, 'USAGE: %s <inputs>' % progname
+	sys.exit(int(fatal))
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		usage(sys.argv[0], True)
+
+	try: files = matchfiles(sys.argv[1:])
+	except IOError as e:
+		print >> sys.stderr, 'ERROR:', e
+
+	for f in files:
+		wset = WaveformSet.fromfile(f)
+		obase = os.path.splitext(f)[0]
+		print 'Extracting backscatter waves from file', f
+		for rx in wset.rxidx:
+			wset[rx,rx].store(obase + '.Element%05d.backscatter' % rx)
---
+++ shell/wset-tgcextract.py
@@ -0,0 +1,33 @@
+#!/opt/python-2.7.9/bin/python
+
+import os, sys, numpy as np
+from habis.formats import WaveformSet
+from habis.habiconf import matchfiles
+
+def usage(progname=None, fatal=True):
+	progname = progname or sys.argv[0]
+	print >> sys.stderr, 'USAGE: %s <inputs>' % progname
+	sys.exit(int(fatal))
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		usage(sys.argv[0], True)
+
+	try: files = matchfiles(sys.argv[1:])
+	except IOError as e:
+		print >> sys.stderr, 'ERROR:', e
+
+	for f in files:
+		wset = WaveformSet.fromfile(f)
+		oname = os.path.splitext(f)[0] + '.tgc.txt'
+
+		try:
+			tgc = np.fromstring(wset.extrabytes['tgc'], dtype=np.float32)
+		except (KeyError, AttributeError):
+			print 'File', f, 'specifies no TGC bytes'
+		except Exception as e:
+			print >> sys.stderr, 'ERROR:', e
+		else:
+			print 'Extracting TGC parameters from file', f, 'to file', oname
+			np.savetxt(oname, tgc)
