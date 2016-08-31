d68cae9a29c6f5fd05fec7393e8d58711d501f11
Andrew J. Hesford <ajh@sideband.org>
2016-08-31 16:23:07 -0400

    Initial pass at HABIS node status script.

---
+++ shell/habistat.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2015 Andrew J. Hesford. All rights reserved.
+# Restrictions are listed in the LICENSE file distributed with this package.
+
+import os
+import uptime
+import yaml
+import glob
+
+def habistat():
+	'''
+	Return, as a YAML block-formatted string, the system uptime, the load
+	average, and a list of identified HABIS devices.
+	'''
+	try:
+		utime = uptime.uptime()
+		if not utime: raise ValueError
+	except Exception as e:
+		utime = 'Indeterminate'
+	else:
+		tstr = ''
+		days, utime = utime // 86400, utime % 86400
+		if days: tstr += '%dd' % (days,)
+
+		hours, utime = utime // 3600, utime % 3600
+		if hours: tstr += '%dh' % (hours,)
+
+		minutes, utime = utime // 60, utime % 60
+		if minutes: tstr += '%dm' % (minutes,)
+
+		if utime or not tstr: tstr += '%0.2fs' % (utime,)
+
+		utime = tstr
+
+	try:
+		loadavgs = os.getloadavg()
+	except Exception as e:
+		loadavgs = 'Indeterminate'
+
+	hadevs = sorted(glob.glob('/dev/habis[0-9]*'))
+
+	stats = { 'Uptime': utime, 'Load averages': loadavgs, 'HABIS devices': hadevs }
+
+	return yaml.safe_dump(stats, default_flow_style=False)
+
+
+if __name__ == '__main__':
+	try:
+		stats = habistat()
+	except Exception as e:
+		stats = 'Unable to query HABIS status ' + str(e)
+	print stats
