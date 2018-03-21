43f54da22e082d3e1364817a5cbf3a35d3498301
Andrew J. Hesford <ajh@sideband.org>
2018-03-20 23:45:15 -0400

    Eliminate some unused and unmaintained functionality.

--- shell/wset-tgcextract.py
+++ shell/wset-tgcextract.py
@@ -4,8 +4,6 @@ import os, sys, numpy as np
 from habis.formats import WaveformSet
 from habis.habiconf import matchfiles
 
-import gzip, bz2
-
 def usage(progname=None, fatal=True):
 	progname = progname or sys.argv[0]
 	print('USAGE: %s <inputs>' % progname, file=sys.stderr)
@@ -23,18 +21,9 @@ if __name__ == '__main__':
 
 	for f in files:
 		oname, fext = os.path.splitext(f)
-
-		if fext.lower() == '.gz':
-			oname = os.path.splitext(oname)[0]
-			ff = gzip.open(f, 'rb')
-		elif fext.lower() == '.bz2':
-			oname = os.path.splitext(oname)[0]
-			ff = bz2.open(f, 'rb')
-		else: ff = open(f, 'rb')
-
 		oname += '.tgc.txt'
 
-		wset = WaveformSet.fromfile(ff, header_only=True)
+		wset = WaveformSet.fromfile(f, header_only=True)
 
 		try:
 			tgc = wset.context['tgc']
